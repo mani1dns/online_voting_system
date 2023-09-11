6fd670becfb93bf3059fcff36e4cbe1f3a778ae6
Mani A <mani.msd1357@gmail.com>
2023-09-11 08:33:45 +0530

    registration page is created

--- vote/home/views.py
+++ vote/home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import RegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -9,5 +11,22 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['password'] == cd['confirm_password']:
+                obj = form.save(commit=False)
+                obj.set_password(obj.password)
+                obj.save()
+                form.save()
+                messages.success(request, 'You have been registered successfully.')
+                return redirect('index')
+                return render(request, 'index.html')
+            else:
+                return render(request, "register.html", {'form':form,'note':'password must match'})
+    else:
+        form = RegistrationForm()
+
+    return render(request, "register.html", {'form':form})
     
