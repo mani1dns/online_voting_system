5cce12beba439f86efb24c8f02104c94adbaa37e
Mani A <mani.msd1357@gmail.com>
2023-09-14 19:57:00 +0530

    interface edited

--- vote/home/views.py
+++ vote/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .forms import RegistrationForm
 from .forms import ChangeForm
 from django.contrib.auth import login,logout,authenticate, update_session_auth_hash
